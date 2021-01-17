957e0a640e23d3986b403b67a4093428b0031008
Saman Rohanizade <saman.rohanizade@gmail.com>
2021-01-17 18:35:08 +0330

    define the basic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: ["./src/**/*.html"],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      black: {
+        light: "#050505",
+        DEFAULT: "#000",
+      },
+      indingo: {
+        light: "#007AE1",
+        dark: "#433FD9",
+      },
+      white: "#fff",
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
