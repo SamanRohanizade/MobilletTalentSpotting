80ebdb000872f010d2d9e7f1e020c0b09e4e655e
Saman Rohanizade <saman.rohanizade@gmail.com>
2021-01-18 02:26:02 +0330

    implement special effect for footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,18 @@ module.exports = {
       blue: "#067DE5",
       white: "#fff",
     },
+    boxShadow: {
+      DEFAULT:
+        "0 14px 28px rgba(255,255,255,0.19), 0 10px 10px rgba(255,255,255,0.23);",
+    },
     fontFamily: {
       english: ["Helvetica Neue Regular", "sans-serif"],
       persian: ["IRANSans", "sans-serif"],
+    },
+    extend: {
+      zIndex: {
+        negative: "-1",
+      },
     },
   },
 };
