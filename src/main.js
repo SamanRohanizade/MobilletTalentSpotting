eae93a7291782520fb9f53a61646202c2f704494
Saman Rohanizade <saman.rohanizade@gmail.com>
2021-03-07 14:00:57 +0330

    implement simple accordion

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Snap from "snapsvg";
+import Snap from 'snapsvg';
 
 // it's OK to wrap the event with a promise in this special case
 // cause it's a one-off event
 const loadedPr = new Promise(resolve => {
-  window.addEventListener("load", resolve);
+  window.addEventListener('load', resolve);
 });
 
 const minimumWaitPr = new Promise(resolve => setTimeout(resolve, 1650));
@@ -12,18 +12,29 @@ const minimumWaitPr = new Promise(resolve => setTimeout(resolve, 1650));
 Promise.all([loadedPr, minimumWaitPr]).then(revealContent);
 
 function revealContent() {
-  const mask = document.getElementById("mask");
-  const maskContent = document.getElementById("mask-content");
-  const unRevealedPath = Snap.select("#revealed");
-  const revealedPath = Snap.select("#un-revealed");
-  const revealedPathPoints = revealedPath.node.getAttribute("d");
+  const mask = document.getElementById('mask');
+  const maskContent = document.getElementById('mask-content');
+  const unRevealedPath = Snap.select('#revealed');
+  const revealedPath = Snap.select('#un-revealed');
+  const revealedPathPoints = revealedPath.node.getAttribute('d');
   const reveal = () => {
     unRevealedPath.animate({ d: revealedPathPoints }, 4300, mina.backout);
   };
   reveal();
 
-  maskContent.classList.add("slide-up");
-  maskContent.addEventListener("transitionend", () => {
+  maskContent.classList.add('slide-up');
+  maskContent.addEventListener('transitionend', () => {
     mask.remove();
   });
 }
+
+(function setupAccordions() {
+  const toggles = Array.from(
+    document.querySelectorAll('[data-toggle="accordion"]')
+  );
+  toggles.forEach(toggle =>
+    toggle.addEventListener('click', e => {
+      document.querySelector(e.target.dataset.opens).classList.toggle('hidden');
+    })
+  );
+})();
