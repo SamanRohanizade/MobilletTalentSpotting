c333176a976752f63e348069f2fb2f9f3ef4f95f
Saman Rohanizade <saman.rohanizade@gmail.com>
2021-03-07 14:00:42 +0330

    change formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,27 @@
 module.exports = {
-  purge: ["./src/*.html"],
+  purge: ['./src/*.html'],
   theme: {
     colors: {
       black: {
-        light: "#050505",
-        DEFAULT: "#000",
+        'x-light': '#292929',
+        light: '#050505',
+        DEFAULT: '#000',
       },
-      blue: "#067DE5",
-      white: "#fff",
+      blue: '#067DE5',
+      white: '#fff',
     },
     boxShadow: {
       DEFAULT:
-        "0 14px 28px rgba(255,255,255,0.19), 0 10px 10px rgba(255,255,255,0.23);",
+        '0 14px 28px rgba(55,255,155,0.19), 0 10px 10px rgba(55,255,155,0.23);',
+      'dark-inset': 'inset 0px 18px 18px 0px rgba(0,0,0,0.32);',
     },
     fontFamily: {
-      english: ["Helvetica Neue Regular", "sans-serif"],
-      persian: ["IRANSans", "sans-serif"],
+      english: ['Helvetica Neue Regular', 'sans-serif'],
+      persian: ['IRANSans', 'sans-serif'],
     },
     extend: {
       zIndex: {
-        negative: "-1",
+        negative: '-1',
       },
     },
   },
